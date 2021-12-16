9099fbb6910b924cf456e747ebec0f2ac8e29afd
hongju <litehj84@gmail.com>
2021-12-16 18:00:28 +0900

    회원 서비스 로직

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,54 @@
+// 비즈니스 메소드 (서비스)
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // test 코드와 동일 객체 사용하기 위해 constructor 사용
+    // dependency injection
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+        // ifPresent(): null 아닌 어떤 값이 있으면 내부 로직이 동작
+        // Optional 이기 때문에 가능한 method
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+        // Optional 반환하는 코드보단 아래 코드 형태를 많이 쓴다.
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
